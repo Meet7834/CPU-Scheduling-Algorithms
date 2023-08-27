31f0e7fcdf7dd1c2b625150e1965a99774f4aacd
Meet <proobmeet7834@gmail.com>
2023-08-28 03:05:05 +0530

    Modified code to be structured code

--- FCFS.c
+++ FCFS.c
@@ -1,10 +1,63 @@
 #include<stdio.h>
+#include <stdlib.h>
 
-int findMinIndex(int arrivalTime[], int length){
+// Defination of functions:
+struct myProcess createProcess();
+void printArr();
+void printProcess();
+int findMinIndex();
+void FCFS();
+
+//Structure:
+struct myProcess{
+    int numProcess;
+    int idleCPUTime; // Amount of time CPU is being idle for the process sequence.
+    int timeSpent; // Amount of time to execute the all of the processes
+    int arrivalTime[100]; // Arrival Time of the process
+    int burstTime[100]; // Burst Time (execution time of the process)
+    int completionTime[100]; // The point in time at which the process was done being executed
+    int turnAroundTime[100]; // The amount of time that process spent in the CPU
+    // TurnAroundTime = CompletionTime for the process - ArrivalTime of the process
+    int waitingTime[100]; // The amount of time spent by process in the waiting queue
+};
+
+// Constructor & Other Class-Like Functions:
+
+struct myProcess createProcess(int numProcess) {
+    struct myProcess newProcess;
+
+    newProcess.numProcess = numProcess;
+    newProcess.idleCPUTime = 0;
+    newProcess.timeSpent = 0;
+
+    return newProcess;
+}
+
+void printProcess(struct myProcess *pr){
+    printf("\nTotal time spent on running these process(s): %d", pr->timeSpent);
+    printf("\nTotal idle CPU time: %d", pr->idleCPUTime);
+
+    printf("\nCompletion Time for each process: ");
+    printArr(pr->completionTime, pr->numProcess);
+    
+    printf("\nTurn Around Time for each process: ");
+    printArr(pr->turnAroundTime, pr->numProcess);
+    
+    printf("\nWaiting Time for each process: ");
+    printArr(pr->waitingTime, pr->numProcess);
+}
+
+//Utility Functions:
+void printArr(int arr[], int length){
+    printf("\n");
+    for (int i=0; i<length; i++) printf("%d ", arr[i]);
+}
+
+int findMinIndex(int arr[], int length){
     int min = __INT_MAX__, index = length;
 
-    for (int i=0; i<length; i++) if (min > arrivalTime[i] && arrivalTime[i] != -1) {
-        min = arrivalTime[i];
+    for (int i=0; i<length; i++) if (min > arr[i] && arr[i] != -1) {
+        min = arr[i];
         index = i;
     }
 
@@ -14,17 +67,18 @@ int findMinIndex(int arrivalTime[], int length){
     return index;
 }
 
-void printArr(int arr[], int length){
-    printf("\n");
-    for (int i=0; i<length; i++) printf("%d ", arr[i]);
-}
+// Main Algorithms:
 
-void FCFS(int numProcess, int arrivalTime[], int burstTime[]){
-    int timeSpent = 0, idleCPUTime = 0;
-    int completionTime[numProcess], turnAroundTime[numProcess], waitingTime[numProcess];
+void FCFS(struct myProcess *pr){
+    int numProcess = pr->numProcess;
+    int timeSpent = pr->timeSpent;
+    int idleCPUTime = pr->idleCPUTime;
+    int *arrivalTime = pr->arrivalTime;
+    int *turnAroundTime = pr->turnAroundTime;
+    int *completionTime = pr->completionTime;
+    int *burstTime = pr->burstTime;
+    int *waitingTime = pr->waitingTime;
     
-    // TurnAroundTime = Completion time of the process - Arvial Time of the process
-    // WaitingTime = TurnAroundTime of the process - Burst Time of the process
 
     while(findMinIndex(arrivalTime, numProcess) != -1){
         int minIndex = findMinIndex(arrivalTime, numProcess);
@@ -37,24 +91,12 @@ void FCFS(int numProcess, int arrivalTime[], int burstTime[]){
         }
 
         timeSpent += burstTime[minIndex]; // This is the time that process is running
-        turnAroundTime[minIndex] = timeSpent - arrivalTime[minIndex]; // 
+        turnAroundTime[minIndex] = timeSpent - arrivalTime[minIndex]; 
         completionTime[minIndex] = turnAroundTime[minIndex] + arrivalTime[minIndex];
         waitingTime[minIndex] = turnAroundTime[minIndex] - burstTime[minIndex];
 
         arrivalTime[minIndex] = -1;
     }
-
-    printf("\nTotal time spent on running these process(s): %d", timeSpent);
-    printf("\nTotal idle CPU time: %d", idleCPUTime);
-
-    printf("\nCompletion Time for each process: ");
-    printArr(completionTime, numProcess);
-    
-    printf("\nTurn Around Time for each process: ");
-    printArr(turnAroundTime, numProcess);
-    
-    printf("\nWaiting Time for each process: ");
-    printArr(waitingTime, numProcess);
 }
 
 int main(){
@@ -67,12 +109,16 @@ int main(){
     scanf("%d", &numProcess);
     int arrivalTime[numProcess], burstTime[numProcess];
 
+    struct myProcess myPr = createProcess(numProcess);
+    
     printf("\nEnter arrival time of the process: ");
-    for(int i=0; i<numProcess; i++) scanf("%d", &arrivalTime[i]);
+    for(int i=0; i<numProcess; i++) scanf("%d", &myPr.arrivalTime[i]);
     
     printf("\nEnter burst time of the process: ");
-    for(int i=0; i<numProcess; i++) scanf("%d", &burstTime[i]);
+    for(int i=0; i<numProcess; i++) scanf("%d", &myPr.burstTime[i]);
 
-    FCFS(numProcess, arrivalTime, burstTime);
-
+    // Call to FCFS function will calculate all the required numbers and then the printProcess function will print all the arrays and needed numbers.
+    FCFS(&myPr);
+    printProcess(&myPr);
+    
 }
