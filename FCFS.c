32fae7a229b5944efbdcce9fae8048cf541465e4
Meet <proobmeet7834@gmail.com>
2023-08-28 01:52:18 +0530

    First working model of FCFS

--- FCFS.c
+++ FCFS.c
@@ -7,15 +7,57 @@ int findMinIndex(int arrivalTime[], int length){
         min = arrivalTime[i];
         index = i;
     }
-    if (index == length) return -1;
+
+    // if it returns index as -1 it means the all the process are executed.
+    if (index == length) return -1;             
+    
     return index;
 }
 
+void printArr(int arr[], int length){
+    printf("\n");
+    for (int i=0; i<length; i++) printf("%d ", arr[i]);
+}
+
 void FCFS(int numProcess, int arrivalTime[], int burstTime[]){
-    int timeSpent = 0, waitingTime = 0;
+    int timeSpent = 0, idleCPUTime = 0;
+    int completionTime[numProcess], turnAroundTime[numProcess], waitingTime[numProcess];
+    
     while(findMinIndex(arrivalTime, numProcess) != -1){
         int minIndex = findMinIndex(arrivalTime, numProcess);
+        
+        if (arrivalTime[minIndex] == timeSpent){ // Process doesn't have to wait for its turn waiting time will be zero;
+            
+        }
+        else if (arrivalTime[minIndex] < timeSpent){ //Process will have to wait for other process to finish:
+            
+        }
+        else if (arrivalTime[minIndex] > timeSpent){ // CPU will be idle untill the next process arrives:
+            idleCPUTime += arrivalTime[minIndex] - timeSpent;
+            timeSpent += arrivalTime[minIndex] - timeSpent;
+            
+        }
+
+        timeSpent += burstTime[minIndex];
+        turnAroundTime[minIndex] = timeSpent - arrivalTime[minIndex];
+        completionTime[minIndex] = turnAroundTime[minIndex] + arrivalTime[minIndex];
+        waitingTime[minIndex] = turnAroundTime[minIndex] - burstTime[minIndex];
+
+        arrivalTime[minIndex] = -1;
     }
+
+    printf("\nTotal time spent on running these process(s): %d", timeSpent);
+    printf("\nTotal idle CPU time: %d", idleCPUTime);
+
+    printf("\nCompletion Time for each process: ");
+    printArr(completionTime, numProcess);
+    
+    printf("\nTurn Around Time for each process: ");
+    printArr(turnAroundTime, numProcess);
+    
+    printf("\nWaiting Time for each process: ");
+    printArr(waitingTime, numProcess);
+
 }
 
 int main(){
@@ -34,6 +76,11 @@ int main(){
     printf("\nEnter burst time of the process: ");
     for(int i=0; i<numProcess; i++) scanf("%d", &burstTime[i]);
 
+    FCFS(numProcess, arrivalTime, burstTime);
 
-
+    /*Sample Test case:
+    4
+    0 1 5 6
+    2 2 3 4
+    */
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CPU-Scheduling-Algorithms
-This program contains the algorithms to calculate the waiting time and total time spent for all of the processes to be completed.
+This program contains the algorithms to calculate the waiting time and total time spent for all of the processes to be completed. This program assumes that CPU is only running one process at a time.
 
 ## FCFS: First Come First Serve:
-- Working of the algorithm:
-    -- In this algo the cpu runs whichever process came first. This algo doesn't care if the CPU is utilizing the time efficiently or not.
+- Working of the algorithm: (Mode: "Non-PreEmptive")
+     In this algo the cpu runs whichever process came first. This algo doesn't care if the CPU is utilizing the time efficiently or not.
