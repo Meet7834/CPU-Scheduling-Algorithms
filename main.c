ed2fc1a1200899215621c30db0d3bfb636c825ce
Meet <proobmeet7834@gmail.com>
2023-09-03 22:58:37 +0530

    modified main.c file for better UX

--- main.c
+++ main.c
@@ -9,52 +9,50 @@
 #include "algos/SRTF.h"
 
 int main() {
-//    int algo = 0;
-//    while (algo != -1) {
-//        int numProcess, timeQuantum;
-//        printf("\nEnter the scheduling algorithm: 1: FCFS, 2: SJF, 3: RR: ");
-//        scanf("%d", &algo);
-//
-//        if (algo > 3 || algo < 1) {
-//            printf("\nPlease enter the correct algorithm.");
-//            continue;
-//        } else if (algo >= 1 && algo <= 3) {
-//            printf("\nEnter the number of the process: ");
-//            scanf("%d", &numProcess);
-//            int arrivalTime[numProcess], burstTime[numProcess];
-//
-//            printf("\nEnter arrival time of the process: ");
-//            for (int i = 0; i < numProcess; i++) scanf("%d", &arrivalTime[i]);
-//
-//            printf("\nEnter burst time of the process: ");
-//            for (int i = 0; i < numProcess; i++) scanf("%d", &burstTime[i]);
-//
-//            if (algo == 1 || algo == 2) {
-//                struct myProcess pr = createProcess(numProcess, arrivalTime, burstTime);
-//                if (algo == 1) {
-//                    FCFS(&pr);
-//                    printProcess(&pr);
-//                } else if (algo == 2) {
-//                    SJF(&pr);
-//                    printProcess(&pr);
-//                }
-//            } else if (algo == 3) {
-//                printf("\nEnter time quantum:");
-//                scanf("%d", &timeQuantum);
-//                struct myProcess pr = createProcessPreEmptive(numProcess, arrivalTime, burstTime, timeQuantum);
-//                roundRobin(&pr);
-//                printProcessPreEmptive(&pr);
-//            }
-//        } else {
-//            printf("\nWrong Input, Exiting Program.");
-//            break;
-//        }
-//        printf("\n\nTo exit enter -1 or to continue enter 1");
-//        scanf("%d", &algo);
-//    }
-    int arrivalTime[5] = {3, 1, 4, 0, 20};
-    int burstTime[5] = {1, 4, 2, 6, 3};
-    struct myProcess pr = createProcess(5, arrivalTime, burstTime);
-    SRTF(&pr);
-    printProcessPreEmptive(&pr);
+    int algo = 0;
+    while (algo != -1) {
+        int numProcess, timeQuantum;
+        printf("\nEnter the scheduling algorithm: \n1: FCFS, 2: SJF, 3: SRTF, 4: RR: \n");
+        scanf("%d", &algo);
+
+        if (algo > 4 || algo < 1) {
+            printf("\nPlease enter the correct algorithm.");
+            continue;
+        } else if (algo >= 1 && algo <= 4) {
+            printf("\nEnter the number of the process: ");
+            scanf("%d", &numProcess);
+            int arrivalTime[numProcess], burstTime[numProcess];
+
+            printf("\nEnter arrival time of the process: ");
+            for (int i = 0; i < numProcess; i++) scanf("%d", &arrivalTime[i]);
+
+            printf("\nEnter burst time of the process: ");
+            for (int i = 0; i < numProcess; i++) scanf("%d", &burstTime[i]);
+
+            if (algo >=1 && algo <= 3) {
+                struct myProcess pr = createProcess(numProcess, arrivalTime, burstTime);
+                if (algo == 1) {
+                    FCFS(&pr);
+                    printProcess(&pr);
+                } else if (algo == 2) {
+                    SJF(&pr);
+                    printProcess(&pr);
+                }else if (algo == 3){
+                    SRTF(&pr);
+                    printProcessPreEmptive(&pr);
+                }
+            } else if (algo == 4) {
+                printf("\nEnter time quantum:");
+                scanf("%d", &timeQuantum);
+                struct myProcess pr = createProcessPreEmptive(numProcess, arrivalTime, burstTime, timeQuantum);
+                roundRobin(&pr);
+                printProcessPreEmptive(&pr);
+            }
+        } else {
+            printf("\nWrong Input, Exiting Program.");
+            break;
+        }
+        printf("\n\nTo exit enter -1 or to continue enter 1: ");
+        scanf("%d", &algo);
+    }
 }
--- run_linux.sh
+++ run_linux.sh
@@ -5,9 +5,11 @@ gcc -c structures/heap.c -o heap.o
 gcc -c structures/queue.c -o queue.o
 gcc -c algos/FCFS.c -o FCFS.o
 gcc -c algos/SJF.c -o SJF.o
+gcc -c algos/SRTF.c -o SRTF.o
 gcc -c algos/RoundRobin.c -o RoundRobin.o
 
-gcc main.c utils.o myProcess.o heap.o queue.o FCFS.o SJF.o RoundRobin.o -o my_program
+gcc main.c utils.o myProcess.o heap.o queue.o FCFS.o SJF.o RoundRobin.o SRTF.o -o my_program
+rm utils.o heap.o queue.o RoundRobin.o myProcess.o FCFS.o SJF.o SRTF.o
 ./my_program
 
-rm utils.o heap.o queue.o RoundRobin.o myProcess.o FCFS.o SJF.o my_program
+rm my_program
